c8e9e0e131af4118c19ce0378041da6730dd7f4d
nidhichougule <nidhichougule20@gmail.com>
2026-06-27 00:12:32 +0530

    Time: 0 ms (100%), Space: 8.6 MB (69.98%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long power=n;
+        if(power<0){
+            x=1/x;
+            power=-power;
+        }
+        if (power==0) return 1;
+        double half=myPow(x,power/2);
+        if(power%2==0) return half*half;
+        else return half*half*x;
+    }
+};
