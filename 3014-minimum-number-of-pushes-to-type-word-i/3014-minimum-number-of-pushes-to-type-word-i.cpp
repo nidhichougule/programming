22961f1efc46ae64ba1d4f294cc199e286680a8b
Nidhi Santosh Chougule <nidhichougule20@gmail.com>
2026-07-31 01:57:00 +0530

    Time: 0 ms (100%), Space: 8.4 MB (54.24%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int n = word.size();
+        int ans = 0;
+        int cost = 1;
+
+        while (n >= 8) {
+            ans += 8 * cost;
+            n -= 8;
+            cost++;
+        }
+
+        ans += n * cost;
+        return ans;
+    }
+};
